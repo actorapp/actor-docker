f5da25d093c98406b864bcaf8709bc6ca5ad5123
Steve Kite <steve@actor.im>
2016-09-27 12:39:30 +0300

    feat: upgrade ubuntu version and fix hostname resolving

--- ethereum/geth/entry_point.sh
+++ ethereum/geth/entry_point.sh
@@ -11,7 +11,7 @@ if [ "${GETH_GENESIS:-}" ]; then
 fi
 
 if [ "${GETH_BOOTNODE_HOST:-}" ]; then
-	BOOTNODE=`host $GETH_BOOTNODE_HOST | awk '/has address/ { print $4 }' | tail -n1`
+	BOOTNODE=`dig +short $GETH_BOOTNODE_HOST | tail -n1`
 	echo "Boot Node found: $BOOTNODE from $GETH_BOOTNODE_HOST"
 	BOOTNODE_ARGS="enode://$GETH_BOOTNODE_KEY@$BOOTNODE:30301"
 	exec /usr/bin/geth --datadir=$GETH_DATA --bootnodes=$BOOTNODE_ARGS "$@"
