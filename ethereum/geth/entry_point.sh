bbc492b985ca2768ee70ffe8ad100a2f6f3fc1dd
Steve Kite <steve@actor.im>
2016-09-27 12:17:18 +0300

    feat(ethereum): Allow joining boot node via hostname

--- ethereum/geth/entry_point.sh
+++ ethereum/geth/entry_point.sh
@@ -10,4 +10,11 @@ if [ "${GETH_GENESIS:-}" ]; then
 	/usr/bin/geth --datadir=$GETH_DATA init $GETH_GENESIS
 fi
 
-exec /usr/bin/geth --datadir=$GETH_DATA "$@"
+if [ "${GETH_BOOTNODE_HOST:-}" ]; then
+	BOOTNODE=`host $GETH_BOOTNODE_HOST | awk '/has address/ { print $4 }' | tail -n1`
+	echo "Boot Node found: $BOOTNODE from $GETH_BOOTNODE_HOST"
+	BOOTNODE_ARGS="enode://$GETH_BOOTNODE_KEY@$BOOTNODE:30301"
+	exec /usr/bin/geth --datadir=$GETH_DATA --bootnodes=$BOOTNODE_ARGS "$@"
+else
+	exec /usr/bin/geth --datadir=$GETH_DATA "$@"
+fi
